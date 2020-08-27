5590a13ad4cec413a533a06aef51cdd9710525e1
Daniel Madrala <dmadrala@wp.pl>
2020-08-28 00:20:52 +0200

    init

---
+++ perceptron/README.md
@@ -0,0 +1,5 @@
+Simple perceptron algorithm implementation
+
+Does work for binary classification assuming that input data is linearly
+separable and labels must be either -1 or 1. Does not work for labels that are 0 or 1.
+
---
+++ perceptron/perceptron.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+class Perceptron:
+
+    def __init__(self):
+        self.w = np.array((0, 0))
+
+    def train(self, x_train, y_train):
+        while(True):
+            m = 0
+            for xi, yi in zip(x_train, y_train):
+                res = np.dot(self.w.T, xi)
+                if yi * res <= 0:
+                    self.w = self.w + yi * xi
+                    m += 1
+            if m == 0:
+                break          
+
+    def predict(self, x):
+        return 1 if np.dot(self.w.T, x) > 0 else -1
+
+    def save_weights(self):
+        with open('weights', 'wb') as file:
+            np.save(file, self.w)
+
+    def load_weights(self, filename):
+        if filename is None:
+            return
+        with open(filename, 'rb') as file:
+            self.w = np.load(file)
+
+if __name__ == "__main__":
+    x = np.array([[-4, 2], [-1, -1], [-2, 4], [2, -1], [-3, 6], [3, 2], [1, 5], [6, 2], [3, 7], [6, 4]])
+    y = np.array([1, -1, 1, -1, 1, -1, 1, -1, 1, -1])
+
+    p = Perceptron()
+    #p.train(x, y)
+    #p.save_weights()
+    #p.load_weights('weights')
+    print(p.predict([1, 8]))
